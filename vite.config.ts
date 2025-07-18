e35b230c057a7c0246e8b42ae33bd9e4ead78c50
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-07-17 21:52:13 -0300

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "https://github.com/germanpgonzalez/dolarito-app",
+   base: '/dolarito-app/',
   plugins: [react(), 
     tailwindcss()
   ],
